0f5b847a31525c3b9e355c125574053e3195745f
TUENGUYEN-190055 <71994219+TUENGUYEN-190055@users.noreply.github.com>
2021-02-08 10:11:13 +0900

    ユーザトップページの作成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item; //追加
+// use App\Http\Requests\ItemRequest; //追加
 
 class HomeController extends Controller
 {
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+       // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //return view('home');
+        $items = Item::limit(10)->get();
+        $data = ['items' => $items];
+        return view('home', $data);
     }
 }
